a83d7be28edb603d7b1424214facd591ba1e1cf1
Alex-Zain <84902847+Alex-Zain@users.noreply.github.com>
2021-09-20 21:44:00 +0530

    Update zk_dec.py

--- zk_dec.py
+++ zk_dec.py
@@ -664,7 +664,8 @@ class crackmenu:
     def api(self, user, zkth):
         global loop
         (
-         sys.stdout.write('\r[\xe2\x9e\xa5] Cracking: %s/%s \xe2\x9e\xa4 OK:%s \xe2\x9e\xa4 CP:%s ' % (loop, len(self.id), len(ok), len(cp))),)        sys.stdout.flush()
+         sys.stdout.write('\r[\xe2\x9e\xa5] Cracking: %s/%s \xe2\x9e\xa4 OK:%s \xe2\x9e\xa4 CP:%s ' % (loop, len(self.id), len(ok), len(cp))),)
+        sys.stdout.flush()
         for pw in zkth:
             pw = pw.lower()
             try:
